a757628c80b01a1c54e7ff94ddb98a536c856167
praveen-elastic <praveen.kukreja@elastic.co>
2022-06-01 12:23:26 +0530

    Indexing of chats

--- ees_zoom/zoom_chat_messages.py
+++ ees_zoom/zoom_chat_messages.py
@@ -14,9 +14,8 @@ import time
 import requests
 from dateutil.relativedelta import relativedelta
 
-from .constant import CHATS, FILES, RFC_3339_DATETIME_FORMAT
-from .utils import extract, retry
-from .zoom_client import ZoomClient
+from .constant import CHATS, RFC_3339_DATETIME_FORMAT
+from .utils import retry
 
 
 class ZoomChatMessages:
@@ -60,7 +59,6 @@ class ZoomChatMessages:
             requests.exceptions.Timeout,
         )
     )
-    @ZoomClient.regenerate_token()
     def get_chats_from_user_id(self, user_id, start_time, end_time):
         """This method is responsible to fetch chats initiated by the user id, which are in range of past
         six months.
@@ -167,159 +165,5 @@ class ZoomChatMessages:
         except Exception as exception:
             self.logger.error(
                 f"Error {exception} occurred while generating chat(s) documents."
-            )
-            raise exception
-
-    @retry(
-        exception_list=(
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout,
-        )
-    )
-    @ZoomClient.regenerate_token()
-    def get_files_from_user_id(self, user_id, start_time, end_time):
-        """This method will fetch all the files sent by the user, save it in the list.
-        :param user_id: string of the user ID.
-        :param start_time: datetime string for Lower limit for data fetching.
-        :param end_time: datetime string for upper limit for data fetching.
-        :returns: list of dictionary containing files of the user.
-        """
-        user_files = []
-        next_page_token = True
-        try:
-            while next_page_token:
-                url = (
-                    f"https://api.zoom.us/v2/chat/users/{user_id}/messages?page_size=300&search_key=%20&"
-                    f"search_type=file&from={start_time}&to={end_time}"
-                )
-                if next_page_token is not True:
-                    url = f"{url}&next_page_token={next_page_token}"
-                headers = {
-                    "authorization": f"Bearer {self.zoom_client.access_token}",
-                    "content-type": "application/json",
-                }
-                files_response = requests.get(url=url, headers=headers)
-                if files_response and files_response.status_code == 200:
-                    response = json.loads(files_response.text)
-                    next_page_token = response["next_page_token"]
-                    user_files.extend(response["messages"])
-                elif files_response.status_code == 401:
-                    if time.time() > self.zoom_client.access_token_expiration:
-                        self.zoom_client.get_token()
-                else:
-                    files_response.raise_for_status()
-        except (
-            requests.exceptions.HTTPError,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout,
-        ) as exception:
-            self.logger.exception(
-                f"Exception raised while fetching file(s) from Zoom: {exception}"
-            )
-            raise exception
-        except Exception as exception:
-            self.logger.exception(
-                f"Unknown error occurred while fetching file(s) from Zoom: {exception}"
-            )
-            raise exception
-        return user_files
-
-    @retry(
-        exception_list=(
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout,
-        )
-    )
-    def fetch_file_content(self, file_download_url):
-        """This method will fetch the file content from file download url using
-        get request from Zoom.
-        :param file_download_url: file download url.
-        :returns: file content.
-        """
-        attachment_content_response = None
-        try:
-            attachment_content_response = requests.get(file_download_url)
-        except (
-            requests.exceptions.HTTPError,
-            requests.exceptions.ConnectionError,
-            requests.exceptions.Timeout,
-        ) as exception:
-            self.logger.exception(
-                f"Exception raised while fetching file content from Zoom: {exception}"
-            )
-        except Exception as exception:
-            self.logger.exception(
-                f"Unknown error occurred while fetching file content from Zoom: {exception}"
-            )
-        return attachment_content_response
-
-    def get_files_details_documents(
-        self,
-        users_data,
-        files_schema,
-        start_time,
-        end_time,
-        enable_permission,
-    ):
-        """This method will iterate over list of users and will get all the files sent by user,
-        it will create files documents from the returned data ready to be indexed.
-        :param users_data: list of dictionary contains Users details.
-        :param files_schema: dictionary of fields to be indexed for Files.
-        :param start_time: datetime object for lower limit for data fetching.
-        :param end_time: datetime object for upper limit for data fetching.
-        :param enable_permission: boolean to check if permission sync is enabled or not.
-        :returns: dictionary containing type of data along with the data.
-        """
-        try:
-            start_time, end_time = self.get_time_range(
-                start_time, end_time
-            )
-            files_documents = []
-            for user in users_data:
-                self.logger.info(
-                    f"Thread: [{threading.get_ident()}] Attempting to extract file(s) for user {user}."
-                )
-                file_list = self.get_files_from_user_id(user, start_time, end_time)
-                self.logger.info(
-                    f"Thread: [{threading.get_ident()}] Fetched total : {len(file_list)} file(s) for {user}."
-                )
-                for file in file_list:
-                    # skipping the file if it's already fetched by any previous user id.
-                    if any(
-                        document["id"] == file["file_id"]
-                        for document in files_documents
-                    ):
-                        continue
-                    file_document = {"type": FILES, "parent_id": user}
-                    for ws_field, zoom_fields in files_schema.items():
-                        file_document[ws_field] = file[zoom_fields]
-                    attachment_content_response = self.fetch_file_content(
-                        file["download_url"]
-                    )
-                    content = ""
-                    if attachment_content_response:
-                        content = extract(attachment_content_response)
-                    file_document[
-                        "body"
-                    ] = f"Sender : {file['sender']}\nFile Content : {content}"
-                    if enable_permission:
-                        permission_list = ["ChatMessage:Read"]
-                        permission_list.extend(
-                            self.zoom_enterprise_search_mappings.get(user, [])
-                        )
-                        file_document["_allow_permissions"] = permission_list
-                    files_documents.append(file_document)
-            self.logger.info(
-                f"Thread: [{threading.get_ident()}] Fetched total {len(files_documents)} file(s) documents."
-            )
-            return {"type": FILES, "data": files_documents}
-        except KeyError as key_error_exception:
-            self.logger.error(
-                f"Error {key_error_exception} occurred while generating file(s) documents."
-            )
-            raise key_error_exception
-        except Exception as exception:
-            self.logger.error(
-                f"Error {exception} occurred while generating file(s) documents."
             )
             raise exception
