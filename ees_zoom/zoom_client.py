3f35f414706b76c18c56d0bb174569144d4ae346
Praveen Kukreja <90465691+praveen-elastic@users.noreply.github.com>
2022-06-02 21:15:48 +0530

    remove unnecessary change

--- ees_zoom/zoom_client.py
+++ ees_zoom/zoom_client.py
@@ -70,8 +70,8 @@ class ZoomClient:
         )
     )
     def get_token(self):
-        """This module generates access token and refresh token using stored refresh token.
-        If refresh token is not stored then uses authorization code."""
+        """This module generates access token and refresh token using stored refresh token.If refresh token is not stored then uses
+        authorization code."""
         lock.acquire()
         if time.time() < self.access_token_expiration:
             lock.release()
