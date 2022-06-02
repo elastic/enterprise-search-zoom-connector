ee808500474c0f3efc8ecb1357a33d25b05c4d9e
praveen-elastic <praveen.kukreja@elastic.co>
2022-06-02 18:23:47 +0530

    optimization for documents storage updation

--- ees_zoom/full_sync_command.py
+++ ees_zoom/full_sync_command.py
@@ -63,8 +63,6 @@ class FullSyncCommand(BaseCommand):
                 queue,
                 self.zoom_enterprise_search_mappings,
             )
-            local_storage = self.local_storage
-            storage_with_collection = local_storage.get_storage_with_collection()
             partitioned_users_lists = sync_zoom.get_all_users_from_zoom()
             global_keys_for_roles = sync_zoom.perform_sync(ROLES, [{}])
             global_keys = self.create_jobs(
@@ -78,12 +76,6 @@ class FullSyncCommand(BaseCommand):
                 if object_type in [ROLES, GROUPS]:
                     continue
                 queue.put_checkpoint(object_type, current_time, INDEXING_TYPE)
-            try:
-                for global_key in global_keys:
-                    if global_key not in storage_with_collection["global_keys"]:
-                        storage_with_collection["global_keys"].append(global_key)
-            except ValueError as value_error:
-                self.logger.error(f"Exception while updating storage: {value_error}")
 
             # Send end signals for each live threads to notify them to close watching the queue
             # for any incoming documents
@@ -97,16 +89,17 @@ class FullSyncCommand(BaseCommand):
             )
             raise exception
 
-        return storage_with_collection
+        return global_keys
 
-    def start_consumer(self, queue, storage_with_collection):
+    def start_consumer(self, queue, global_keys):
         """This method starts async calls for the consumer which is responsible for indexing documents to the
         Enterprise Search.After successful indexing it stores checkpoints of time dependent objects and updates
         the doc_id according to indexed documents.
         :param queue: Shared queue to fetch the stored documents
-        :param storage_with_collection: doc ids of the fetched documents.
+        :param global_keys: updated list of dictionary for local storage documents.
         """
         checkpoint = Checkpoint(self.config, self.logger)
+        storage_with_collection = self.local_storage.get_storage_with_collection()
         thread_count = self.config.get_value("enterprise_search_sync_thread_count")
         sync_es = SyncEnterpriseSearch(
             self.config, self.logger, self.workplace_search_client, queue
@@ -123,13 +116,13 @@ class FullSyncCommand(BaseCommand):
         self.logger.info(
             f"SUMMARY : Total {len(total_documents_indexed)} documents indexed out of {len(total_documents_generated)}"
         )
-        # For loop removes the documents which were not indexed to Enterprise search from local storage.
-        for index in range(len(total_documents_indexed)):
-            if (
-                storage_with_collection["global_keys"][index]["id"]
-                not in total_documents_indexed
-            ):
-                del storage_with_collection["global_keys"][index]
+        try:
+            # for loop removes the documents which were not indexed to Enterprise search from local storage.
+            for global_key in global_keys:
+                if global_key not in storage_with_collection["global_keys"] and global_key["id"] in total_documents_indexed:
+                    storage_with_collection["global_keys"].append(global_key)
+        except ValueError as value_error:
+            self.logger.error(f"Exception while updating storage: {value_error}")
 
         self.local_storage.update_storage(storage_with_collection)
 
@@ -138,6 +131,6 @@ class FullSyncCommand(BaseCommand):
         current_time = get_current_time()
         self.logger.info(f"Indexing started at: {current_time}")
         queue = ConnectorQueue(self.logger)
-        storage_with_collection = self.start_producer(queue)
-        self.start_consumer(queue, storage_with_collection)
+        global_keys = self.start_producer(queue)
+        self.start_consumer(queue, global_keys)
         self.logger.info(f"Indexing ended at: {get_current_time()}")
--- ees_zoom/incremental_sync_command.py
+++ ees_zoom/incremental_sync_command.py
@@ -49,8 +49,6 @@ class IncrementalSyncCommand(BaseCommand):
                 queue,
                 self.zoom_enterprise_search_mappings,
             )
-            local_storage = self.local_storage
-            storage_with_collection = local_storage.get_storage_with_collection()
             partitioned_users_lists = sync_zoom.get_all_users_from_zoom()
             global_keys_for_roles = sync_zoom.perform_sync(ROLES, [{}])
             global_keys = self.create_jobs(
@@ -64,12 +62,6 @@ class IncrementalSyncCommand(BaseCommand):
                 if object_type in [ROLES, GROUPS]:
                     continue
                 queue.put_checkpoint(object_type, current_time, INDEXING_TYPE)
-            try:
-                for global_key in global_keys:
-                    if global_key not in storage_with_collection["global_keys"]:
-                        storage_with_collection["global_keys"].append(global_key)
-            except ValueError as value_error:
-                self.logger.error(f"Exception while updating storage: {value_error}")
 
             # Send end signals for each live threads to notify them to close watching the queue
             # for any incoming documents
@@ -83,16 +75,17 @@ class IncrementalSyncCommand(BaseCommand):
             )
             raise exception
 
-        return storage_with_collection
+        return global_keys
 
-    def start_consumer(self, queue, storage_with_collection):
+    def start_consumer(self, queue, global_keys):
         """This method starts async calls for the consumer which is responsible for indexing documents to the
         Enterprise Search.After successful indexing it stores checkpoints of time dependent objects and updates
         the doc_id according to indexed files.
         :param queue: Shared queue to fetch the stored documents
-        :param storage_with_collection: doc ids of the fetched documents.
+        :param global_keys: updated list of dictionary for local storage documents.
         """
         checkpoint = Checkpoint(self.config, self.logger)
+        storage_with_collection = self.local_storage.get_storage_with_collection()
         thread_count = self.config.get_value("enterprise_search_sync_thread_count")
         sync_es = SyncEnterpriseSearch(
             self.config, self.logger, self.workplace_search_client, queue
@@ -109,13 +102,13 @@ class IncrementalSyncCommand(BaseCommand):
         self.logger.info(
             f"SUMMARY : Total {len(total_documents_indexed)} documents indexed out of {len(total_documents_generated)}"
         )
-        # For loop removes the documents which were not indexed to Enterprise search from local storage.
-        for index in range(len(total_documents_indexed)):
-            if (
-                storage_with_collection["global_keys"][index]["id"]
-                not in total_documents_indexed
-            ):
-                del storage_with_collection["global_keys"][index]
+        try:
+            # for loop removes the documents which were not indexed to Enterprise search from local storage.
+            for global_key in global_keys:
+                if global_key not in storage_with_collection["global_keys"] and global_key["id"] in total_documents_indexed:
+                    storage_with_collection["global_keys"].append(global_key)
+        except ValueError as value_error:
+            self.logger.error(f"Exception while updating storage: {value_error}")
 
         self.local_storage.update_storage(storage_with_collection)
 
@@ -147,6 +140,6 @@ class IncrementalSyncCommand(BaseCommand):
             ]
             objects_time_range[object_type] = start_time_end_time_list
         queue = ConnectorQueue(self.logger)
-        storage_with_collection = self.start_producer(queue, objects_time_range)
-        self.start_consumer(queue, storage_with_collection)
+        global_keys = self.start_producer(queue, objects_time_range)
+        self.start_consumer(queue, global_keys)
         self.logger.info(f"Indexing ended at: {get_current_time()}")
